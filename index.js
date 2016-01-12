ce4fd70d074ca553ec611b44849c6a3e0ccf01fb
Jorge Capona <jcapona@gmail.com>
2016-01-11 23:47:40 -0300

    Gets requested info using 'request headers'

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ app.set('port', (process.env.PORT || 5000));
 app.set('view engine', 'jade');
 
 app.get("/", function(request, response) {
-  response.render("index");
+  
+  var ip = request.header('x-forwarded-for') || request.connection.remoteAddress;
+  var lang = request.header('accept-language').split(",")[0];
+  var os = request.header('user-agent').match(/\(([^)]+)\)/)[1];
+    
+  response.send(JSON.stringify({"ipaddress": ip,"language": lang,"software":os}));
+
 });
 
 app.listen(app.get('port'));
